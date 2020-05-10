60d623f2c10df6d7d7267e0230262ebdf5001d37
seanchen7 <seanchensq@gmail.com>
2020-05-10 15:42:46 -0400

    updates

--- fico_heloc_data.py
+++ fico_heloc_data.py
@@ -3,68 +3,31 @@
 import os
 import numpy as np
 import pandas as pd
+# Scikit-learn package for machine learning models
 import sklearn as sk
 from sklearn import tree
+from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import RandomizedSearchCV
-
+# Packages for graphics
 import pydotplus
 from IPython.display import Image
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Change directory
-file_path = os.path.dirname(os.path.realpath('fico_heloc_data'))
+file_path = '/Users/sean/Documents/GitHub/FICO-Challenge'
 os.chdir(file_path)
-
-#%% User-defined functions
-
-# Print summary for out-of-sample model perforamce
-def model_summary(y_test, tc_predict, dtc_cv_score):
-    print("=== Confusion Matrix ===")
-    print(confusion_matrix(y_test, dtc_predict))
-    print('\n')
-    print("=== Classification Report ===")
-    print(classification_report(y_test, dtc_predict))
-    print('\n')
-    print("=== All AUC Scores ===")
-    print(dtc_cv_score)
-    print('\n')
-    print("=== Mean AUC Score ===")
-    print("Mean AUC Score - Random Forest: ", dtc_cv_score.mean())
-
-# Search for best depth parameter for decision tree model
-def tree_depth_search (X_train, X_test, y_train, y_test, criterion, depth_range):
-    dt2 = pd.DataFrame()
-    for i in depth_range:
-        
-        clf = tree.DecisionTreeClassifier(criterion=criterion, max_depth=i, random_state=66)
-        clf = clf.fit(X_train,y_train)
-        out_sample_pred = clf.predict(X_test)
-        in_sample_pred = clf.predict(X_train)
-        in_acc = sk.metrics.accuracy_score(y_train, in_sample_pred)
-        out_acc = sk.metrics.accuracy_score(y_test, out_sample_pred)
-        in_conf = sk.metrics.confusion_matrix(y_train, in_sample_pred)
-        out_conf = sk.metrics.confusion_matrix(y_test, out_sample_pred)
-        tpr_in = in_conf[1][1]/(in_conf[1][0]+in_conf[1][1])
-        tpr_out = out_conf[1][1]/(out_conf[1][0]+out_conf[1][1])
-        fpr_in = in_conf[0][1]/(in_conf[0][0]+in_conf[0][1])
-        fpr_out = out_conf[0][1]/(out_conf[0][0]+out_conf[0][1])
-        
-        dt1 = pd.DataFrame({'Depth': [i, i], 'Sample': ['In-Sample', 'Out-Sample'], 'Accuracy': [in_acc, out_acc],
-                            "TPR": [tpr_in,tpr_out], "FPR": [fpr_in, fpr_out]})
-        dt2 = pd.concat([dt2, dt1])
-   
-    return dt2
-
+# os. getcwd() 
 
 #%% Import and prepare data
 
 # FICO Home Equity Line of Credit (HELOC) Dataset (https://community.fico.com/s/explainable-machine-learning-challenge)
 df = pd.read_csv(file_path + '/Data/heloc_dataset_v1.csv')
+df.dtypes
 
 # The target variable to predict is a binary variable called RiskPerformance. 
 target_variable = 'RiskPerformance'
@@ -78,17 +41,19 @@ df['Class'] = df['Class'].astype(int)
 df.groupby('Class')['Class'].count() # Count of categorical variable
 
 # Convert categorical variables to proper types
-df.dtypes
 var_category = [target_variable]  #Keep 'Class' as numerical since it only has two values
 df[var_category] = df[var_category].astype('category')
 
+# Feature variables
+features = list(df.columns)
+features.remove(target_variable)
+features.remove('Class')
 
 #%% Data cleaning 
 
 # Data cleaning and exclusion
 ## Special values according to data dictionary
 df[df==-9].count() # -9 No Bureau Record or No Investigation
-df = df[df['ExternalRiskEstimate']!=-9] #Exclude records 
 
 dt1 = df[df==-8].count() # -8 No Usable/Valid Trades or Inquiries
 dt1[dt1>0]
@@ -136,48 +101,80 @@ df.groupby('MaxDelqEver')['MaxDelqEver'].count()
 
 #%% Data exploration
 
-# Feature variables
-feature_variables = list(df.columns)
-feature_variables.remove(target_variable)
-feature_variables.remove('Class')
+#Exclude records with missing values
+df = df[df['ExternalRiskEstimate']!=-9] 
 
 # Data summary 
 ## Descriptive stats for numerical variables
-df_summary = df[['Class'] + feature_variables].describe()
+df_summary = df[['Class'] + features].describe()
 ## Descriptive stats for categorical variables
 df_summary2 = df.groupby(var_category)[var_category].count()
-
+## Group mean values
+df_summary3 = df.groupby('Class').mean()
+df_summary4 = df.groupby('ExternalRiskEstimate').mean()
 ## Correlations for numerical variables
-df_corr = df[['Class'] + feature_variables].corr() #Point Biserial Correlation btw Class and feature variables
-
+df_corr = df[['Class'] + features].corr() #Point Biserial Correlation btw Class and feature variables
 ## Top correlated feature variables
-feature_corr = pd.DataFrame({'corr': df_corr['Class'], 'abs_corr': abs(df_corr['Class'])})
-feature_corr.sort_values(by = 'abs_corr', ascending=False, inplace=True)
-feature_corr.head(10) 
+df_corr2 = pd.DataFrame({'corr': df_corr['Class'], 'abs_corr': abs(df_corr['Class'])})
+df_corr2.sort_values(by = 'abs_corr', ascending=False, inplace=True)
+features_sub = list(df_corr2.iloc[1:16].index)
 
-## Output to excel
-# with pd.ExcelWriter('output_data_summary.xlsx') as writer:  
-#     df_summary.to_excel(writer, sheet_name='Summary_Numeric')
-#     df_summary2.to_excel(writer, sheet_name='Summary_Category')
-#     df_corr.to_excel(writer, sheet_name='Corr1')
-#     feature_corr.to_excel(writer, sheet_name='Corr2')
+# Top features for considerations (correlations in parenthesis):
+# ExternalRiskEstimate	-0.46
+# NetFractionRevolvingBurden	0.33
+# PercentTradesWBalance	0.28
+# PercentTradesNeverDelq	-0.26
+# AverageMInFile	-0.25
+# MaxDelq2PublicRecLast12M	-0.24
+# MaxDelqEver	-0.22
+# MSinceOldestTradeOpen	-0.20
+# NumTrades60Ever2DerogPubRec	0.17
+# NumInqLast6M	0.14
+# NumSatisfactoryTrades	-0.14
+# PercentInstallTrades	0.14
+# NumTrades90Ever2DerogPubRec	0.14
+# NumInqLast6Mexcl7days	0.14
+# MSinceMostRecentInqexcl7days	-0.12
+
+
+# Correlation matrix for select variables
+df_corr3 = df[features_sub].corr() 
+
+### Output to excel
+with pd.ExcelWriter('output_data_summary.xlsx') as writer:  
+    df_summary.to_excel(writer, sheet_name='Summary_Numeric')
+    df_summary2.to_excel(writer, sheet_name='Summary_Category')
+    df_summary4.to_excel(writer, sheet_name='Summary_Group_Mean')
+    df_corr.to_excel(writer, sheet_name='Corr1')
+    df_corr2.to_excel(writer, sheet_name='Corr2')
+    df_corr3.to_excel(writer, sheet_name='Corr3')
 
 ## Other correlation methods
 # from scipy import stats
-# stats.f_oneway(df.loc[df[target_variable]==target_names[0], feature_variables[0]],
-#                df.loc[df[target_variable]==target_names[1], feature_variables[0]]) #ANOVA when Class has two or more values
-# stats.pearsonr(df[feature_variables[0]], df[feature_variables[1]]) #Pearson’s Correlation
-# stats. spearmanr(df[feature_variables[0]], df[feature_variables[1]]) #Spearman’s Correlation
+# stats.f_oneway(df.loc[df[target_variable]==target_names[0], features[0]],
+#                df.loc[df[target_variable]==target_names[1], features[0]]) #ANOVA when Class has two or more values
+# stats.pearsonr(df[features[0]], df[features[1]]) #Pearson’s Correlation
+# stats. spearmanr(df[features[0]], df[features[1]]) #Spearman’s Correlation
 
 
 #%% Data visualization
 
+df2 = df[[target_variable] + features_sub].copy() #Data subset for plotting
+
 # Box plot
-sns.boxplot(x="MaxDelqEver", y=target_variable, data=df)
-# sns.catplot(x=target_variable, y="MaxDelqEver", kind="box", data=df)
+sns.boxplot(x=features_sub[0], y=target_variable, data=df)
+## Single boxplot with multiple subplots for each variable
+n_rows = 5
+n_cols = 3
+fig, axes = plt.subplots(nrows=n_rows, ncols=n_cols, figsize=(20, 20))
+for i in list(range(0, n_rows)):
+    for j in list(range(0, n_cols)):
+        k = (i*n_cols)+j
+        if k<len(features_sub):
+            sns.boxplot(x=features_sub[k], y=target_variable, data=df, ax=axes[i, j])
 
 
-#Structure multi-plots
+#Structure multi-plots based on groups
 col1 = "MaxDelq2PublicRecLast12M"
 var_x = "ExternalRiskEstimate"
 var_y = "PercentTradesNeverDelq"
@@ -188,7 +185,7 @@ g.add_legend();
 
 
 #Pairwise plot
-vars1 = ['ExternalRiskEstimate', 'NetFractionRevolvingBurden', 'PercentTradesNeverDelq']
+vars1 = ['ExternalRiskEstimate', 'NetFractionRevolvingBurden', 'AverageMInFile']
 
 g = sns.PairGrid(df[vars1+[target_variable]], hue=target_variable)
 g.map_diag(plt.hist)
@@ -196,15 +193,93 @@ g.map_offdiag(plt.scatter)
 g.add_legend();
 
 
-#%% Feature selections
+#%% User-defined functions
+
+# Print summary for out-of-sample model perforamce
+def model_summary(y_test, dtc_predict, dtc_cv_score):
+    print("=== Confusion Matrix ===")
+    print(confusion_matrix(y_test, dtc_predict))
+    print('\n')
+    print("=== Classification Report ===")
+    print(classification_report(y_test, dtc_predict))
+    print('\n')
+    print("=== All AUC Scores ===")
+    print(dtc_cv_score)
+    print('\n')
+    print("=== Mean AUC Score ===")
+    print("Mean AUC Score - Random Forest: ", dtc_cv_score.mean())
+
+# Search for best depth parameter for decision tree model
+def tree_depth_search (X_train, X_test, y_train, y_test, criterion, depth_range):
+    dt2 = pd.DataFrame()
+    for i in depth_range:
+        
+        clf = tree.DecisionTreeClassifier(criterion=criterion, max_depth=i, random_state=66)
+        clf = clf.fit(X_train,y_train)
+        out_sample_pred = clf.predict(X_test)
+        in_sample_pred = clf.predict(X_train)
+        in_acc = sk.metrics.accuracy_score(y_train, in_sample_pred)
+        out_acc = sk.metrics.accuracy_score(y_test, out_sample_pred)
+        in_conf = sk.metrics.confusion_matrix(y_train, in_sample_pred)
+        out_conf = sk.metrics.confusion_matrix(y_test, out_sample_pred)
+        tpr_in = in_conf[1][1]/(in_conf[1][0]+in_conf[1][1])
+        tpr_out = out_conf[1][1]/(out_conf[1][0]+out_conf[1][1])
+        fpr_in = in_conf[0][1]/(in_conf[0][0]+in_conf[0][1])
+        fpr_out = out_conf[0][1]/(out_conf[0][0]+out_conf[0][1])
+        
+        dt1 = pd.DataFrame({'Depth': [i, i], 'Sample': ['In-Sample', 'Out-Sample'], 'Accuracy': [in_acc, out_acc],
+                            "TPR": [tpr_in,tpr_out], "FPR": [fpr_in, fpr_out]})
+        dt2 = pd.concat([dt2, dt1])
+   
+    return dt2
+
+
+#%% Logisitic Regressions
 
 #Define predcitors and target variable
-X = df[feature_variables].copy() #Change default here if we need to drop some variables
+X = df[features_sub].copy() 
 y = df['Class'].copy()
 
 # Implement train-test-split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=66)
 
+
+from sklearn.feature_selection import RFE
+
+lge = LogisticRegression(solver='liblinear', random_state=66)
+lge = RFE(lge, 20)
+lge = lge.fit(X_train, y_train)
+
+print(lge.support_)
+print(lge.ranking_)
+
+
+lge_predict = lge.predict(X_test) # predictions
+lge_cv_score = cross_val_score(lge, X, y, cv=10, scoring='roc_auc') #scores
+model_summary(y_test, lge_predict, lge_cv_score) # Model summary
+
+
+import statsmodels.api as sm
+logit_model=sm.Logit(y_train,X_train)
+logit_model_result=logit_model.fit()
+print(logit_model_result.summary2())
+
+# ROC Curve
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+logit_roc_auc = roc_auc_score(y_test, lge.predict(X_test))
+fpr, tpr, thresholds = roc_curve(y_test, lge.predict_proba(X_test)[:,1])
+plt.figure()
+plt.plot(fpr, tpr, label='Logistic Regression (area = %0.2f)' % logit_roc_auc)
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic')
+plt.legend(loc="lower right")
+plt.savefig('Log_ROC')
+plt.show()
 
 #%% Decision tree model
 
@@ -216,7 +291,7 @@ dtc_cv_score = cross_val_score(dtc, X, y, cv=10, scoring='roc_auc') #scores
 model_summary(y_test, dtc_predict, dtc_cv_score) # Model summary
 
 ## Plot the tree
-dot_data = tree.export_graphviz(dtc, feature_names=feature_variables, class_names=target_names, out_file=None, filled = True)
+dot_data = tree.export_graphviz(dtc, feature_names=features, class_names=target_names, out_file=None, filled = True)
 graph = pydotplus.graph_from_dot_data(dot_data)
 Image(graph.create_png())
 # graph.write_png("tree_model1.png")
@@ -243,7 +318,7 @@ dtc_cv_score = cross_val_score(dtc, X, y, cv=10, scoring='roc_auc') #scores
 model_summary(y_test, dtc_predict, dtc_cv_score) # Model summary
 
 ## Plot the tree
-dot_data = tree.export_graphviz(dtc, feature_names=feature_variables, class_names=target_names, out_file=None, filled = True)
+dot_data = tree.export_graphviz(dtc, feature_names=features, class_names=target_names, out_file=None, filled = True)
 graph = pydotplus.graph_from_dot_data(dot_data)
 Image(graph.create_png())
 # graph.write_png("tree_model2.png")
